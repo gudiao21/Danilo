7569a6171d08e31a20cec953b0b9616b44fa68e6
gudiao21 <lucianodantas53@gmail.com>
2023-01-28 20:58:27 -0300

    Committing files

--- seu-joao.rb
+++ seu-joao.rb
@@ -1,8 +1,14 @@
-require 'byebug'
+#Programa para calcular desconto para o cliente com base no valor total da compra.
+
+system 'clear'
+puts "Fala, seu João! Tudo bem com o senhor?"
 puts "Qual é o valor total do pedido: "
 purchase = gets.to_f
 puts "Qual o valor da porcentagem de desconto: "
 perc = gets.to_f
-discount = purchase * perc
-result = purchase - discount
-puts result
+discount_value = purchase * perc/100
+result = purchase - discount_value
+puts "Seu João, o valor total digitado foi: R$#{purchase}"
+puts "O valor do desconto foi #{discount_value}"
+puts "Valor total a ser cobrado é de: R$#{result}"
+puts "=============================="
