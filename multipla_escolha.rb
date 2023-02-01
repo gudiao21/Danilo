3f7f2010afc344e0e97081819a69a3332c2d2677
gudiao21 <lucianodantas53@gmail.com>
2023-02-01 16:39:33 -0300

    Committing multipla_escolha.rb

--- multipla_escolha.rb
+++ multipla_escolha.rb
@@ -1,20 +1,35 @@
+# Dado que eu tenha algumas opções:
+# - Banana
+# -Maça
+# -Laranja
+# e digite algum dos itens listados acima, ao escolher, coloque um valor diferente para fruta, este valor estará no bloco selecionada no final do programa mostre o valor de acordo com a escolha da fruta.
+
 require 'byebug'
 
 puts "Digite um número: "
 x = gets.to_i
 
 debugger
-# if (1..5).include?(x)
-#     puts "It's between 1 and 5"
-# elsif x == 6
-#     puts "It's 6"
-# elsif [10,30].include?(x)
-#     puts "It's either 10 or 30"
-# elsif x == 50
-#     puts "It's 50"
-# else
-#     puts "You gave me #{x} -- I have no idea what to do with that."              
-# end
+
+if (x > 5 && x < 10) || (x >= 50 && x<= 78)
+    puts "maior do que 5 e menor do que 10" 
+end
+
+if "danilo" =~ /.*?lo/ #"=~" indica uma comparação, ".*?lo" indica: qualquer coisa até "lo".
+    puts "Um if com regex"
+end
+
+if (1..5).include?(x)
+    puts "It's between 1 and 5"
+elsif x == 6
+    puts "It's 6"
+elsif [10,30].include?(x)
+    puts "It's either 10 or 30"
+elsif x == 50
+    puts "It's 50"
+else
+    puts "You gave me #{x} -- I have no idea what to do with that."              
+end
 
 
 case x
