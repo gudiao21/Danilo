682940fd43cdc42e6385b342696e4963629ed335
gudiao21 <lucianodantas53@gmail.com>
2023-02-02 19:16:40 -0300

    Committing loop.rb file

--- loop.rb
+++ loop.rb
@@ -1,12 +1,13 @@
+#Obs: Programa feito pelo professor Danilo na aula (06- Loop e Array).
 #Enquanto estamos na aula, o Danilo estará explicando lógica de programação e desenvolvimento pessoal. Aluno, você concorda com isso?
 
 notas = []
 em_aula = true
 
 while em_aula do
- # system "clear"
+  system "clear"
   puts "Qual a nota que você daria para este atendimento (0..10)?"
-  nota << gets.to_f
+  nota = gets.to_f
   break if nota == -1
   notas << nota
   #puts "A aula já acabou ? (S/N)"
