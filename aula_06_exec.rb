5a44a21c42ca9beebd7079423eb661c64861eb0e
gudiao21 <lucianodantas53@gmail.com>
2023-02-03 20:03:57 -0300

    Committing aula_06_exec.rb file

--- aula_06_exec.rb
+++ aula_06_exec.rb
@@ -1,9 +1,10 @@
 #Exercício passado pelo Danilo do (06-Loop e Array).
 # Danilo é o fundador do "torne-se um programador" e ele precisa calcular a satisfação de seus alunos. Faça um programa que calcule o NPS do seu negócio.
 
+system 'clear'
 notas = []
 #pesquisa_em_andamento = true
-data_encerramento = Time.now + 10
+data_encerramento = Time.now + 10 #Seria 10 segundos.
 puts data_encerramento
 puts Time.now
 #require 'activesupport'
@@ -11,8 +12,10 @@ puts Time.now
 #debugger
 while Time.now < data_encerramento do
   puts "Qual é a nota de (0..10) você daria para a vigente aula?"
+  puts "========================="
+  print 'Digite a nota: '
   nota = gets.to_f
-  puts "============================"
+  #puts "============================"
   #break if nota == -1
   unless (1..10).include?(nota)
     puts "A nota atribuída não pode ser processada, pois está fora da margem de possibilidades!"
@@ -26,14 +29,15 @@ while Time.now < data_encerramento do
   if data_encerramento > Time.now
     puts "A pesquisa irá encerrar em: #{Time.at((data_encerramento - Time.now)).strftime("%M:%S")}"
   else
-    puts "A pesquisa se encerrou!"  
+    puts "A pesquisa se encerrou!"
+    puts "======================"
   end
+  sleep(2)
+  system 'clear'
 end  
 puts "=========================="
 puts "RESULTADO DA AVALIAÇÃO:"
 puts "=========================="
 puts "A quantidade de avaliações(ão) (foi/foram) de: #{notas.length}"
-puts "=========================="
 puts "Notas obtidas: #{(notas)}"
-puts "=========================="
-puts "O seu NPS é de: #{(notas.sum / notas.length)}"
+puts "O seu NPS é de: #{(notas.sum / notas.length)}
--- teste.rb
+++ teste.rb
@@ -1,16 +1,43 @@
-notas = []
-pesquisa_em_andamento = true
+require 'byebug'
 
-while pesquisa_em_andamento
-  puts "Qual a nota entre (0..10) que você dá para essa aula vigente?"
-  nota = gets.to_f
-  break if nota == -1
-  if (1..10).include?(nota)
-    notas << nota
-  else
-    #system 'clear'
-    puts "O valor fornecido não coincide com o padrão possível!"
-  end
-end
+# top = 4
+# now = 0
+# puts "while #{now += 1} .. #{top}" until now >= top
 
-puts "O valor do NPS é de: #{(notas.sum / notas.length)}"
+# index = 0
+# loop do
+#   debugger
+#   index +=1
+#   next if index == 5 || index == 8
+#   puts index
+#   break if index == 10
+# end
+#===============================================
+# 8.times do |i|
+#   next if i % 2 == 0 # Se na comparação eu adicionar "!=", vai mostrar os pares.
+#   puts i
+# end
+#==============================================
+# for i in 0..7
+#   next if i % 2 == 0 #Next se for par.
+#   puts i #Imprimirá apenas ímpares.
+# end
+#==============================================
+# (1..7).each do |i|
+#   next if i % 2 ==0
+#   puts i
+# end
+#=============================================
+# a = [1,2,3,4,5].select{|i| i > 4}
+# puts a.to_s
+#=============================================
+a = [1,2,3,4,5].map{|i| "Oi - #{i}"}
+puts a.to_s
+
+puts "======================================="
+
+a = [1,2,3,4,5].map{|i| "Oi - #{i}"}
+
+a.each do |i|
+  puts i
+end  
