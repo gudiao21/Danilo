4cdef21e2e8f46af3efedaf629cb175ac35d9fa2
gudiao21 <lucianodantas53@gmail.com>
2023-02-03 15:12:56 -0300

    Committing aula_06_exec.rb file

--- aula_06_exec.rb
+++ aula_06_exec.rb
@@ -3,7 +3,7 @@
 
 notas = []
 #pesquisa_em_andamento = true
-data_encerramento = Time.now + 60
+data_encerramento = Time.now + 10
 puts data_encerramento
 puts Time.now
 #require 'activesupport'
@@ -12,15 +12,28 @@ puts Time.now
 while Time.now < data_encerramento do
   puts "Qual é a nota de (0..10) você daria para a vigente aula?"
   nota = gets.to_f
+  puts "============================"
   #break if nota == -1
+  unless (1..10).include?(nota)
+    puts "A nota atribuída não pode ser processada, pois está fora da margem de possibilidades!"
+  end  
   if (1..10).include?(nota) #O número que eu estou querendo está incluso neste "range"?
     notas << nota
-    puts "Sua nota foi computada."
+  end
+  
+  puts "Sua nota foi computada com sucesso!"
+
+  if data_encerramento > Time.now
+    puts "A pesquisa irá encerrar em: #{Time.at((data_encerramento - Time.now)).strftime("%M:%S")}"
   else
-    puts "A nota atribuída não pode ser processada, pois está fora da margem de possibilidades!"
+    puts "A pesquisa se encerrou!"  
   end
-  puts "A pesquisa irá encerrar às: #{Time.at((data_encerramento - Time.now)).strftime("M%:%S")}"
-  end
+end  
+puts "=========================="
+puts "RESULTADO DA AVALIAÇÃO:"
+puts "=========================="
 puts "A quantidade de avaliações(ão) (foi/foram) de: #{notas.length}"
+puts "=========================="
 puts "Notas obtidas: #{(notas)}"
+puts "=========================="
 puts "O seu NPS é de: #{(notas.sum / notas.length)}"
