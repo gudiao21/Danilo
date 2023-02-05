79e481e60d0bf60dff980a473f11e346fa19923f
gudiao21 <lucianodantas53@gmail.com>
2023-02-05 10:29:04 -0300

    Committing hash.rb file

--- hash.rb
+++ hash.rb
@@ -1,11 +1,50 @@
 #Roberto é dono de uma faculdade, ele irá precisar organizar a grade do curso em alguns meses. Faça um programa que organize os dados do semestre da faculdade do Roberto. O que teremos nesta organização: "nome da matéria", "tema até o mês 6". Colocar nesta estrutura pelo menos 3 matérias.
 
-grade = {}
-grade <<{
+grade = []
+grade << {
   "Matemática": [
     "Mês 1 - Título",
     "Mês 2 - Título",
     "Mês 3 - Título",
     "Mês 4 - Título",
-    "Mês 5 - Título"
+    "Mês 5 - Título",
+    "Mês 6 - Título",
+  ]
 }
+
+grade << {
+    "Português": [
+       "Mês 1 - Título",
+       "Mês 2 - Título",
+       "Mês 3 - Título",
+       "Mês 4 - Título",
+       "Mês 5 - Título",
+       "Mês 6 - Título",
+]
+}
+
+grade << {
+    "Ciências": [
+       "Mês 1 - Título",
+       "Mês 2 - Título",
+       "Mês 3 - Título",
+       "Mês 4 - Título",
+       "Mês 5 - Título",
+       "Mês 6 - Título",
+]
+}
+
+puts grade[0][:"Matemática"]
+
+grade = [
+  {
+    "Matemática": [
+      "Mês 1 - Título",
+      "Mês 2 - Título"
+      "Mês 3 - Título",
+      "Mês 4 - Título",
+      "Mês 5 - Título",
+      "Mês 6 - Título",
+    ]
+  }
+]
