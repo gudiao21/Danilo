1c714fdbc04519decdc47606b3ab18d537ae0d38
gudiao21 <lucianodantas53@gmail.com>
2023-02-05 17:39:52 -0300

    Committing hash_array.rb file

--- hash_array.rb
+++ hash_array.rb
@@ -6,9 +6,33 @@
 # Faça o programa em app console utilizando variáveis, loop, condicionais e array.
 
 
+escolha = 0
+cadastro = {}
 
 puts "Escolha uma das opções abaixo: "
 puts "==============================================="
 puts "Digite 1 para cadastrar o aluno."
 puts "Digite 2 para cadastrar a matrícula do aluno."
 puts "Digite 3 para cadastrar a nota do aluno."
+puts "Digite 4 para sair."
+
+case escolha
+  when 1
+    print "Digite o nome do aluno: "
+    cadastro[:nome] = gets.chomp.to_s.strip
+  when 2
+    print "Digite a matrícula do aluno: "
+    cadastro[:matricula] = gets.chomp.to_i.strip
+  when 3
+    print "Digite a nota do aluno: "
+    cadastro[:nota] = gets.chomp.to_f.strip
+  when 4
+    break if escolha == 4
+  else
+    puts "Sua entrada é inválida, entre com um número entre 1 e 4, por favor!"
+  end
+
+  puts "================================================================="
+  puts "A média das notas dos alunos é #{[:notas.sum] / [:nome.length]}"
+  print "Muito obrigado por se cadastrar."
+
