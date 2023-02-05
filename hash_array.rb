4f38b6529eb9896d4635908b1f0425c5aaaa71e2
gudiao21 <lucianodantas53@gmail.com>
2023-02-05 19:31:33 -0300

    Committing hash_array.rb file

--- hash_array.rb
+++ hash_array.rb
@@ -6,32 +6,40 @@
 # Faça o programa em app console utilizando variáveis, loop, condicionais e array.
 
 
+alunos = {}
 escolha = 0
-cadastro = {}
 
-puts "Escolha uma das opções abaixo: "
-puts "==============================================="
-puts "Digite 1 para cadastrar o aluno."
-puts "Digite 2 para cadastrar a matrícula do aluno."
-puts "Digite 3 para cadastrar a nota do aluno."
-puts "Digite 4 para sair."
 
-case escolha
-  when 1
-    print "Digite o nome do aluno: "
-    cadastro[:nome] = gets.chomp.to_s.strip
-  when 2
-    print "Digite a matrícula do aluno: "
-    cadastro[:matricula] = gets.chomp.to_i.strip
-  when 3
-    print "Digite a nota do aluno: "
-    cadastro[:nota] = gets.chomp.to_f.strip
-  when 4
-    break if escolha == 4
-  else
-    puts "Sua entrada é inválida, entre com um número entre 1 e 4, por favor!"
-  end
+while escolha != 4 do
+   system 'clear'
+   puts "Escolha uma das opções abaixo: "
+   puts "==============================================="
+   puts "Digite 1 para cadastrar o aluno."
+   puts "Digite 2 para cadastrar a matrícula do aluno."
+   puts "Digite 3 para cadastrar a nota do aluno."
+   puts "Digite 4 para sair."
+   puts "==============================================="
+   escolha = gets.to_i
+  
+   case
+     when 1
+       print "Digite o nome do aluno: "
+       alunos[nome:gets.to_s.strip]
+       puts "Cadastro feito com sucesso!"
+       sleep(4)
+     when 2
+       print "Digite a matrícula do aluno: "
+       alunos[matricula:gets.chomp.to_i]
+     when 3
+       print "Digite a nota do aluno: "
+       alunos[nota:gets.chomp.to_f]
+     #when 4
+       # break
+     else
+       puts "Sua entrada é inválida, entre com um número entre 1 e 4, por favor!"
+    end
 
+end
   puts "================================================================="
   puts "A média das notas dos alunos é #{[:notas.sum] / [:nome.length]}"
   print "Muito obrigado por se cadastrar."
