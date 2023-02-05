eb08762e88e7420b3a5c18960d335db82075d838
gudiao21 <lucianodantas53@gmail.com>
2023-02-05 16:21:31 -0300

    Committing hash_array.rb file

---
+++ hash_array.rb
@@ -0,0 +1,16 @@
+# Roberto é um diretor de uma escola de música. Ele precisa de um programa que tenha as seguintes opçoes:
+# 1 - Cadastrar aluno (nome, matrícula e notas);
+# 2 - Cadastrar Nota de aluno;
+# 3 - Relatório das notas em média dos alunos;
+# 4 - Sair.
+# Faça o programa em app console utilizando variáveis, loop, condicionais e array.
+
+aluno = [
+  {
+    [
+  nome: "Luciano",
+  matricula: "546852",
+  nota: 10
+  ]
+}
+]
--- teste.rb
+++
@@ -1,4 +0,0 @@
-(1..7).each do |i|
-    next if i % 2 == 0
-    puts i
-end
