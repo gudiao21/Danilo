021424e0608e6490197d29df354cbda74f461599
gudiao21 <lucianodantas53@gmail.com>
2023-02-09 14:39:03 -0300

    Committing exercicios_funcoes.rb file

--- exercicios_funcoes.rb
+++ exercicios_funcoes.rb
@@ -10,21 +10,27 @@ def calculo(numero)
     multiplicador = 1
     puts "A tabuada de #{numero}, é:"
     
-    while multiplicador <= 10 do
-        puts "#{numero} x #{multiplicador} = #{numero * multiplicador}"
-        multiplicador += 1
-        #puts "#{nome}, vamos lá, guerreiros!"
-        #puts "Essa é a taboada do número que você, #{nome}"
-    end    
+        while multiplicador <= 10 do
+            puts "#{numero} x #{multiplicador} = #{numero * multiplicador}"
+            multiplicador += 1
+            #puts "#{nome}, vamos lá, guerreiros!"
+            #puts "Essa é a taboada do número que você, #{nome}"
+        end    
 end    
 
+def chama_guerreiro
+    print "Por favor, antes de te dar o resultado, digite seu nome para que eu saiba para quem farei o cálculo: "
+    @nome = gets.to_s.delete("\n") #"@nome" é uma variável de instância, pois é reconhecida entre objetos, funções e no escopo do código também.
+    puts "#{@nome}, vamos lá, guerreiro!"
+end    
 puts "Bem vindo ao programa de cálculo da tabuada!".upcase
 print "Por favor, digite um 'número inteiro' para que o programa calcule a respectiva tabuada: "
 numero = gets.to_i
-print "Por favor, antes de te dar o resultado, digite seu nome para que eu saiba para quem eu farei o cálculo: "
-nome = gets.to_s.delete("\n")
-puts "#{nome}, vamos lá, guerreiro!"
+chama_guerreiro
+#print "Por favor, antes de te dar o resultado, digite seu nome para que eu saiba para quem eu farei o cálculo: "
+#nome = gets.to_s.delete("\n")
+#puts "#{nome}, vamos lá, guerreiro!"
 
 calculo(numero)
 
-puts "#{nome}, é isso aí! Espero ter te ajudado."
+puts "#{@nome}, é isso aí! Espero ter te ajudado."
