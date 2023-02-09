87aef0029cae3cc30e288a97f210380e33581061
gudiao21 <lucianodantas53@gmail.com>
2023-02-09 09:19:12 -0300

    Committing exercicios_funcoes.rb file

--- exercicios_funcoes.rb
+++ exercicios_funcoes.rb
@@ -1,14 +1,26 @@
 #Danilo é um empresário que trabalha na empresa "Didox Business". Ele precisa de um programador que faça um software que tenha alguns recursos:
 # 1 - Calcular a taboada do número digitado;
-# 2 - Ler um nome e mostrar este nome concatenado com a frase: "Vamos lá guerreiros";
+# 2 - Ler um nome e mostrar este nome concatenado com a frase: "Vamos lá guerreiros!";
 # 3 - Calcular a tabuada e mostrar também a frase acima com o nome.
 
 require 'byebug'
 #debugger
+
+def calculo(numero)
+    multiplicador = 0
+    while multiplicador <= 10 do
+        result = numero * multiplicador
+        puts result
+        #puts "#{nome}, vamos lá, guerreiros!"
+        #puts "Essa é a taboada do número que você, #{nome}"
+    end    
+end    
 
 puts "Bem vindo ao programa de cálculo da tabuada!".upcase
 print "Por favor, digite um 'número inteiro' para que o programa calcule a respectiva tabuada: "
 numero = gets.to_i
 print "Por favor, antes de te dar o resultado, digite seu nome para que eu saiba para quem eu farei o cálculo: "
 nome = gets.to_s
-puts "#{nome}"
+puts "#{nome}, vamos lá, guerreiro:\n\n"
+
+calculo(numero)
