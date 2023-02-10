f18e69a027a67b52489c531754177a78e6047365
gudiao21 <lucianodantas53@gmail.com>
2023-02-10 20:25:45 -0300

    Committing teste.rb file

--- exercicio_oo.rb
+++ exercicio_oo.rb
@@ -1,18 +1,16 @@
-# Luciano Hang é um empresáio do varejo. O mesmo tem uma empresa chamada Havan  e pretende vacinar 25 mil de seus colaboradores. Faça um programa que tenha as características de um colaborador (nome, sobrenome, CPF) e coloqueuma opção neste colaborador marcando se ele já tomou ou não a vacina. Ter um item de consulta para saber se equele CPF foi vacinado.
-Passos:
-1- Cadastrar, porém antes do cadastro, verificar se o CPF já foi vacinado. Se o CPF já foi vacinado, mostrar a mensagem colaborador já tomou a primeira dose. Se não pedir os outros dados do cadastro do colaborador.
+# Luciano Hang é um empresáio do varejo. O mesmo tem uma empresa chamada Havan e pretende vacinar 25 mil de seus colaboradores. Faça um programa que tenha as características de um colaborador (nome, sobrenome, CPF) e coloque uma opção neste colaborador, marcando se ele já tomou ou não a vacina. Ter um item de consulta para saber se aquele CPF foi vacinado.
+# Passos:
+# 1- Cadastrar, porém antes do cadastro, verificar se o CPF já foi vacinado. Se o CPF já foi vacinado, mostrar a mensagem colaborador já tomou a primeira dose. Se não pedir os outros dados do cadastro do colaborador.
 
-class alunos
-    nome
-    matricula
-    notas
-
-    def nome = (value) # Isso é um "setter"
-        @nome = value #Variável de contexto, para o escopo 
-    end
-    
-    def estudar
-
-    end
-
+cpfs = []
+print "Menu:".upcase
+puts "------------------------------------------------------------"
+print "Entre com o CPF para checar se já foi cadastrado: "
+cpf = gets.to_i
+#cpfs << cpf
+if cpfs.include?(cpf)
+    print "O dono desse CPF já foi cadastrado! Tente outro CPF, por favor."
+else
+    cpfs << cpf
+    print "CPF cadastrado com sucesso. Pode ir tomar a sua vacina. "
 end
