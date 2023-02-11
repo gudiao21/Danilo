d55ff522a30c5217dd0fcbd95cd989c456b7b70b
gudiao21 <lucianodantas53@gmail.com>
2023-02-11 08:32:24 -0300

    Committing exercicio_oo.rb file

--- exercicio_oo.rb
+++ exercicio_oo.rb
@@ -1,16 +1,18 @@
 # Luciano Hang é um empresáio do varejo. O mesmo tem uma empresa chamada Havan e pretende vacinar 25 mil de seus colaboradores. Faça um programa que tenha as características de um colaborador (nome, sobrenome, CPF) e coloque uma opção neste colaborador, marcando se ele já tomou ou não a vacina. Ter um item de consulta para saber se aquele CPF foi vacinado.
+
 # Passos:
-# 1- Cadastrar, porém antes do cadastro, verificar se o CPF já foi vacinado. Se o CPF já foi vacinado, mostrar a mensagem colaborador já tomou a primeira dose. Se não pedir os outros dados do cadastro do colaborador.
+# 1- Cadastrar, porém antes do cadastro, verificar se o CPF já foi vacinado. Se o CPF já foi vacinado, mostrar a mensagem: colaborador já tomou a primeira dose. Se não, pedir os outros dados do cadastro do colaborador.
 
 cpfs = []
-print "Menu:".upcase
+puts "Menu:".upcase
 puts "------------------------------------------------------------"
 print "Entre com o CPF para checar se já foi cadastrado: "
 cpf = gets.to_i
 #cpfs << cpf
 if cpfs.include?(cpf)
-    print "O dono desse CPF já foi cadastrado! Tente outro CPF, por favor."
+    print "Colaborador já tomou a primeira dose!"
+    sleep(3)
 else
     cpfs << cpf
-    print "CPF cadastrado com sucesso. Pode ir tomar a sua vacina. "
+    print "CPF cadastrado com sucesso! Pode ir tomar a sua vacina. "
 end
