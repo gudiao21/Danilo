0e6d4c48f1d2c197a1e7c38f541339aa1d328244
gudiao21 <lucianodantas53@gmail.com>
2023-02-12 15:57:20 -0300

    Committing exercicio_oo.rb file

--- teste.rb
+++ teste.rb
@@ -1,21 +1,18 @@
 require 'byebug'
-
 class Carro
-    def nome = (value) #é um setter
-        @nome = value
-    end
+  def nome(value) #é um setter
+    @nome = value
+  end
     
-    def nome
-        @nome
-    end
+  def nome #getter
+    @nome
+  end
     
-    def mostrar
-        puts @nome
-    end
-  
+  def mostrar
+    puts @nome
+  end
 end
-    debugger
-
-    carro = Carro.new
-    carro.nome = "Palio"
-    carro.mostrar
+  debugger
+  carro = Carro.new
+  carro.nome = "Palio"
+  carro.mostrar
