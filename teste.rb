1593826fa68e9b2ba27d798272e6f9555e18bd3a
gudiao21 <lucianodantas53@gmail.com>
2023-02-12 16:26:52 -0300

    Committing exercicio_oo.rb file

--- teste.rb
+++ teste.rb
@@ -1,18 +1,56 @@
 require 'byebug'
 class Carro
-  def nome(value) #é um setter
+  def initialize(nome = "Modelo Padrão") 
+    @nome = nome
+  end
+
+  def nome=(value) #Setter, aqui se altera o valor da variável "@nome".
     @nome = value
   end
-    
-  def nome #getter
+
+  def nome #getter, aqui não se altera o valor da variável "@nome", ficando com o valor definido no Initialize, nome = "Modelo Padrão".
     @nome
   end
-    
-  def mostrar
-    puts @nome
+
+  def pneu=(value)
+    @pneu = value
+  end
+
+  def pneu
+    @pneu
+  end
+  
+  def porta=(value)
+    @porta = value
+  end
+
+  def porta
+    @porta
+  end
+
+  def painel=(value)
+   @painel = value
+  end
+
+  def painel
+    @painel
+  end
+
+  def roda=(value)
+    @roda = value
+  end
+
+  def roda
+    @roda
+  end
+
+  def mostrar(marca="Marca Padrão")
+    puts "Marca: #{marca} - Modelo: #{self.nome}"
   end
 end
-  debugger
-  carro = Carro.new
-  carro.nome = "Palio"
-  carro.mostrar
+
+carro = Carro.new
+
+debugger
+
+Carro.new.mostrar
