9f11d8d76f5a37bd11cee70397c68dfefcac2a80
gudiao21 <lucianodantas53@gmail.com>
2023-02-17 14:05:40 -0300

    Committing receita_bolo.rb file

--- receita_bolo.rb
+++ receita_bolo.rb
@@ -1,5 +1,5 @@
 # Crie um programa que tenha uma receita de bolo, esta receita precisa ter informações sobre o "BOLO FEITO" e informações para "TODOS OS BOLOS". Crie uma classe onde vc terá "PROPRIEDADES DE UM BOLO", o "MÉTODO COZINHAR O BOLO INSTANCIADO" e "COZINHAR VÁRIOS BOLOS" passados por um array.
-# características da classe: NOME, COR, ESPECIAL(S/N)
+# características da classe: NOME, COR, ESPECIAL(S/N).
 # Funções da classe: COZINHAR(para uma única instância), COZINHAR TODOS.
 
 require "byebug"
@@ -8,18 +8,20 @@ class Bolo
     attr_accessor :nome, :cor, :especial #Já cria os "setters" e "getters".
 
     def cozinhar
+        system 'clear'
         print "Digite o nome do bolo: "
         @nome = gets.to_s.strip
         print "Digite a cor do bolo: "
         @cor = gets.to_s.strip
-        Bolo.mostrar_bolo
+        self.mostrar_bolo
     end
 
     def mostrar_bolo
         puts "\n============================="
         puts "Nome do bolo: #{@nome}"
         puts "Cor do bolo: #{@cor}"
-        Bolo.especial_ou_nao
+        #puts "O bolo é especial? #{@especial}"
+        self.especial_ou_nao
         puts "=============================\n"
     end
 
@@ -29,8 +31,10 @@ class Bolo
         opcao = gets.upcase.strip
         case opcao
         when "S"
+            @especial = "Sim"
             puts "Então o bolo é especial! Obaaaa."
         when "N"
+            @especial = "Não"
             puts "Então o bolo não é especial,ou seja, é basicão! Afff."
         else
             puts "Opção não válida, entre com (S) ou (N), por favor!"
