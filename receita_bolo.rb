1bd74147f4889af37872a08d7ae74ddb072e68d0
gudiao21 <lucianodantas53@gmail.com>
2023-02-17 10:21:28 -0300

    Committing receita_bolo.rb file

--- receita_bolo.rb
+++ receita_bolo.rb
@@ -3,3 +3,23 @@
 # Funções da classe: COZINHAR(para uma única instância), COZINHAR TODOS.
 
 class Bolo
+    attr_accessor :nome :cor :especial #Já cria os "setters" e "getters".
+
+    def cozinhar
+        print "Digite o nome do bolo: "
+        puts "Nome: #{@nome}"
+        print "Digite a cor do bolo: "
+        puts "Cor: #{cor}"
+        puts "Especial"
+    end
+
+    def especial_ou_nao
+        opcao = nil
+        print "O bolo é especial ou não? (S/N)"
+        opcao = gets.upcase.strip
+        if opcao == "S"
+            puts "Então o bolo é especial!"
+        else
+            puts "Então esse bolo não é especial, ou seja, é um bolo basicão! Afff."
+        end
+    end
