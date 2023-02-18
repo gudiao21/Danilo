33a8f611192c0c51fdebe4767e6f2e1dd2e1c6df
gudiao21 <lucianodantas53@gmail.com>
2023-02-18 10:40:37 -0300

    Committing receita_bolo.rb file

--- aula-script.sh
+++ aula-script.sh
@@ -1,1 +1,16 @@
-touch criacao-script
+mkdir criado_via_script
+
+touch arquivo-gerado.txt
+
+echo "um teste no arquivo com dado" > arquivo_com_dado.txt
+
+cat <<EOF > mais_um_arquivo.txt
+Estamos aprendendo a manipular
+arquivos na aula de devops
+EOF
+echo "-------------------------------"
+cat arquivo_com_dado.txt
+echo "-------------------------------"
+cat mais_um_arquivo.txt
+chmod +x mais_um_arquivo.txt
+./mais_um_arquivo.txt
