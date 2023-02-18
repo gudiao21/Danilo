e5a9e9b37ae895dcf8f5d341a575d004f475834c
gudiao21 <lucianodantas53@gmail.com>
2023-02-18 15:53:33 -0300

    Committing receita_bolo.rb file

--- aula-script.sh
+++ aula-script.sh
@@ -1,16 +1,15 @@
+cat <<EOF > exercicios_execucao.sh
 mkdir criado_via_script
 
 touch arquivo-gerado.txt
 
 echo "um teste no arquivo com dado" > arquivo_com_dado.txt
-
-cat <<EOF > mais_um_arquivo.txt
-Estamos aprendendo a manipular
-arquivos na aula de devops
-EOF
+echo "Estamos aprendendo a manipular" >> mais_um_arquivo.txt
+echo "arquivos na aula de devops" >> mais_um_arquivo.txt
 echo "-------------------------------"
 cat arquivo_com_dado.txt
 echo "-------------------------------"
 cat mais_um_arquivo.txt
-chmod +x mais_um_arquivo.txt
-./mais_um_arquivo.txt
+EOF
+chmod +x aula-script.sh
+./aula-script.sh
