5a2b88fde0581474deb80b250685cae01054bb65
gudiao21 <lucianodantas53@gmail.com>
2023-02-19 17:47:38 -0300

    Committing exercicio_classes_e_instancias.rb file

--- exercicio_classes_e_instancias.rb
+++ exercicio_classes_e_instancias.rb
@@ -1,4 +1,4 @@
-# Danilo é um dos professores do "Torne-se um programador". Para fixar o conteúdo de orientação a objeto, solicita uma classe onde terá algumas propriedades e métodos.
+# Danilo é um dos professores do "Torne-se um programador". Para fixar o conteúdo de orientação a objeto, solicita uma "classe" onde terá algumas "propriedades e métodos".
 
 
 # classe Animal
@@ -9,5 +9,28 @@
 # Não precisa desenvolver tela, seja simples ... quero só a classe. O que o professor quer é saber se sabemos o que é propriedades, métodos de classe e métodos de instância, não precisando ter nada dentro dos métodos.
 
 class Animal
+    attr_accessor :nome, :raca, :tamanho, :velocidade, :cor, :idade
 
+    def self.buscar_animal_por_id
+    end
+end
+
+class todos_os_animais
+    #def todos_os_animais
+    #end
+
+    def correr
+    end
+    
+    def andar
+    end
+
+    def falar
+    end
+    
+    def relacionar
+    end
+    
+    def buscar_animal_por_nome
+    end
 end
