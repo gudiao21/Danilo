2e49099fe423bf2001bf788e25b6de277d4d2381
gudiao21 <lucianodantas53@gmail.com>
2023-02-20 09:18:34 -0300

    Committing exercicio_classes_e_instancias.rb file

---
+++ exercicio_do_uso_da_classe.rb
@@ -0,0 +1,32 @@
+#Exercicio do uso da classe:
+# Para usar esta classe crie algumas instancias com nomes: leao, cachorro, gato, pato.
+# Utilize os métodos de instância criado neste exemplo.
+# Utilize as funções de classe para buscar os animais. Obs: Não precisa ter o código que busca em memória. Quero somente validar o uso da classe.
+# =============================================================================================
+
+
+class TodosAnimais
+    attr_accessor: id
+    def self.busca_animais_por_id(id)
+
+    end
+end
+
+class animais
+    def leao
+
+    end
+    
+    def cachorro
+
+    end
+
+    def gato
+
+    end
+    
+    def pato
+        
+    end
+
+end
