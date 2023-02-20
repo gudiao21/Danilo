1cf0530fb2d0c5a1c071a9e85a4d589cd5d1209a
gudiao21 <lucianodantas53@gmail.com>
2023-02-20 08:44:05 -0300

    Committing exercicio_classes_e_instancias.rb file

--- exercicio_classes_e_instancias.rb
+++ exercicio_classes_e_instancias.rb
@@ -7,25 +7,26 @@
 
 # Obs:
 # Não precisa desenvolver tela, seja simples ... quero só a classe. O que o professor quer é saber se sabemos o que é propriedades, "métodos de classe" e "métodos de instância", não precisando ter nada dentro dos métodos.
+# =============================================================================================
+# Exercício relacionado ao vídeo: 17|POO - FIXAÇÃO DOS CONCEITOS SOBRE MÉTODOS DE CLASSE:
 
 animais = []
-class Animal
+
+class ControladorAnimal
     attr_accessor :nome, :raca, :tamanho, :velocidade, :cor, :idade, :id #"attr_accessor" é um método que cria, via meta programação, o @nome, @raca, @tamanho, ...
 
-    def self.buscar_animal_por_id(id) #"self" caracteriza um método de classe para todas as instâncias(quando se dá um ".new").
+    def self.buscar_animal_por_id(id) #"self" caracteriza um método de classe para todas as instâncias(quando se dá um ".new")."animal" no singular, por se tratar de apenas um "id"(chave-primária) por animal.
     end
 
-    def self.buscar_animais_por_nome(nome)
+    def self.buscar_animais_por_nome(nome) #"animais" no plural por se tratar da possibilidade de ser mais de um animal com o mesmo nome.
     end
 
-    def relacionar(id)
+    def self.relacionar
     end
 end
 
-class todos_os_animais
-    #def todos_os_animais
-    #end
-
+class TodosAnimais   
+    
     def correr
     end
     
@@ -34,5 +35,10 @@ class todos_os_animais
 
     def latir
     end
-    
+
+    def mostrar
+    end    
 end
+
+# OBS: Às vezes será melhor, para evitar o problema de "merge" e consequentemente de código, será melhor um desacoplamento de funções para melhor integridade do códido, segregando o mesmo.
+# OBS2: Ver sobre"ACOPLAMENTO FORTE" dentro da comunidade, no assunto: "DESAFIO DE ORIENTAÇÃO OBJETO AVANÇADO DO C#".
